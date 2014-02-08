64e057384c9e67b59dabd2b94f8c7a18b0a085d4
book author <author@book.com>
2014-02-08 14:10:25 +0800

    display tieba fans and sogou click from quality; continue to read

--- front/book.php
+++ front/book.php
@@ -7,6 +7,9 @@ $book_id = (int)$book_id;
 list($book_title, $author, $category, $intro, $status, $tieba_follower) = execute_vector("select title, author, category, intro, status, tieba_follower from book where id = $book_id");
 $category = $categories[$category];
 $status = $statuses[$status];
+list($tieba_follower, $sogou_click) = execute_vector("select tieba_fans, sogou_click from quality where title = '$book_title'");
+$tieba_follower = isset($tieba_follower) ? $tieba_follower : 0;
+$sogou_click = isset($sogou_click) ? $sogou_click : 0;
 $_POST['title'] = $book_title;
 $result = mysql_query("select source_id, count(*) as c from chapter where book_id = $book_id group by source_id order by c desc");
 $sources = array();
@@ -19,14 +22,53 @@ foreach ($sources as $source) {
 	$html .= '<li role="presentation"><a role="menuitem" tabindex="-1" href="book.php?id='.$book_id.'&source='.$source.'" target="_self">'.$g_sources[$source].'</a></li>';
 }
 $html .= '</ul></li></ul></div>';
-$html .= "<div class=\"well\" align=\"center\"><strong>类型</strong>：$category &nbsp; <strong>推荐</strong>：$tieba_follower &nbsp; <strong>状态</strong>：$status &nbsp; ";
+$html .= "<div class=\"well\" align=\"center\"><strong>类型</strong>：$category &nbsp; <strong>推荐</strong>：$tieba_follower &nbsp; <strong>粉丝</strong>：$sogou_click &nbsp; <strong>状态</strong>：$status &nbsp; ";
 $html .= '<div class="row"><div class="col-md-8 col-md-offset-2" align="left">'.format_intro($intro).'</div></div>';
 $html .= "</div>";
-$current_source = isset($_GET['source']) ? $_GET['source'] : $sources[0];
+$current_source = $_GET['source'];
+$user_info = get_user_info();
+if (count($user_info) > 0) {
+	$user_id = $user_info['id'];
+	$source_condition = isset($_GET['source']) ? 'and chapter.source_id = "'.$_GET['source'].'"' : '';
+	list($user_source_id, $user_chapter_id) = execute_vector("select chapter.source_id, chapter.id from book, chapter, user_record where user_id = $user_id and book.id = chapter.book_id and chapter_id = chapter.id and book.id = $book_id $source_condition");
+	if (isset($user_source_id)) {
+		$current_source = $user_source_id;
+	}
+}
+if (!isset($current_source)) {
+	$current_source = $sources[0];
+}
 $result = mysql_query("select id, title, source_id from chapter where book_id = $book_id and source_id = '$current_source' order by id");
 $chapters = array();
 while(list($chapter_id, $chapter_title, $source_id) = mysql_fetch_array($result)) {
 	$chapters[] = array($chapter_id, $chapter_title, $source_id);
+}
+if (isset($user_chapter_id)) {
+	$col = 0;
+	$html .= '<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">继续阅读</h3></div>';
+	$html .= '<table class="table table-striped">';
+	$flag = 0;
+	for ($i = 0; $i < count($chapters); ++$i) {
+		list($chapter_id, $chapter_title, $source_id) = $chapters[$i];
+		if ($chapter_id == $user_chapter_id || ($flag > 0 && $flag < 9)) {
+			++$flag;
+		}
+		else {
+			continue;
+		}
+		if ($col % 3 == 0) {
+			$html .= '<tr>';
+		}
+		$html .= '<td>';
+		$html .= "<a href=\"redirect.php?id=$chapter_id\" ref=\"nofollow\">$chapter_title</a> &nbsp; &nbsp;";
+		$html .= '</td>';
+		if ($col % 3 == 2) {
+			$html .= '</tr>';
+		}
+		$col = ++$col % 3;
+	}
+	$html .= '</table>';
+	$html .= '</div>';
 }
 $col = 0;
 $html .= '<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">最新章节</h3></div>';
